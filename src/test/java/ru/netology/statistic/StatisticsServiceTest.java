dc3ebd4a7777af475e911987b053600340d3f421
Maria Alesenko <alesenkomaria@gmail.com>
2020-04-30 16:00:06 -0400

    100% test coverage added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,15 +17,18 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
-    void findMax2() {
+    void findMaxFromTheMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = { 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 12, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
+
 }
